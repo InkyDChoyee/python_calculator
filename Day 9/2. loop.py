e690e15f49df656fcd2048bc778ee9bc4103599d
InkyDChoyee <choyee0421@naver.com>
2023-09-06 00:04:36 +0900

    베이비 동현이의 파이썬 공부 9day

---
+++ Day 9/2. loop.py
@@ -0,0 +1,19 @@
+# while문
+# print(1)
+# print(1+1)
+# print(1+1+1)
+# print(1+1+1+1)
+# print(1+1+1+1+1)
+
+i = 0
+num = 10
+
+print("while 문") # => 참을 만족하는 동안 계속 실행
+while i < num:
+    i = i+ 1
+    print(i)
+
+# print("if 문")  => 참인 경우 한번만 실행하게 됨
+# if i < num:
+#     i = i + 1
+#     print(i)
