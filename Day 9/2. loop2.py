d56005fde90979bd45553ca4c1755b12d92f482d
InkyDChoyee <choyee0421@naver.com>
2023-09-06 00:04:59 +0900

    베이비 동현이의 파이썬 공부 9day

---
+++ Day 9/2. loop2.py
@@ -0,0 +1,20 @@
+num = 0
+while num < 5:
+    num = num + 1
+    print(num)
+else:
+    print("값이 {}이상이므로 종료합니다.".format(num))
+
+print("=============")
+
+num1 = 1
+while num1 <= 5:
+    num1 += 1
+    print(num1)
+
+print("=============")
+
+num2 = 1
+while num2 <= 5:
+    print(num2)
+    num2 += 1
