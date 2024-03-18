dfd99bb6c8d1d099a505449f6eb461aa35e0e8fd
Choyee <143522745+InkyDChoyee@users.noreply.github.com>
2024-03-18 18:02:45 +0900

    Delete Day 9 directory

--- Day 9/2. loop3.py
+++
@@ -1,9 +0,0 @@
-fruits = ["사과", "키위", "바나나", "사과", "바나나", "망고"]
-print(fruits)
-fruit = input("빼낼 과일을 입력해주세요 > ")
-
-while fruit in fruits:
-    fruits.remove(fruit)
-
-print(fruits)
-print("{}를 모두 제거했습니다".format(fruit))
--- Day 9/3. test.py
+++
@@ -1,9 +0,0 @@
-# range 함수
-numbers = list(range(10))
-print(numbers)
-
-numbers2 = list(range(-10, 0)) # 실수형X, 정수형O
-print(numbers2)
-
-numbers3 = list(range(-5, 5, 2)) # step도 사용 가능
-print(numbers3)
